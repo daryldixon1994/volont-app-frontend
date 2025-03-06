33fc251487d03f13d1e5e98d824bae482bbf5a23
daryldixon1994 <kmohamedanouar@gmail.com>
2025-03-06 15:29:31 +0100

    update besURLs

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,10 @@
-export const baseUrl = "http://localhost:5000/api";
-export const baseUserUrl = "http://localhost:5000/api/user";
-export const baseAssoUrl = "http://localhost:5000/api/association";
+export const baseUrl = "https://volont-app-backend.onrender.com/api";
+export const baseUserUrl = "https://volont-app-backend.onrender.com/api/user";
+export const baseAssoUrl =
+  "https://volont-app-backend.onrender.com/api/association";
+// export const baseUrl = "http://localhost:5000/api";
+// export const baseUserUrl = "http://localhost:5000/api/user";
+// export const baseAssoUrl = "http://localhost:5000/api/association";
 export function getToken() {
   return localStorage.getItem("token");
 }
